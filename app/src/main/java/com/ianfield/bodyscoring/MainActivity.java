781d4164dc6082c7eaa68e6b5ecf8558a23bd2fc
Ian Field <ian.field90@gmail.com>
2016-08-06 17:28:27 +0100

    Prepare build

--- app/src/main/java/com/ianfield/bodyscoring/MainActivity.java
+++ app/src/main/java/com/ianfield/bodyscoring/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onEdit(String recordId) {
+                // TODO Go to create screen in edit mode instead
                 Intent intent = new Intent(MainActivity.this, ScoringActivity.class);
                 intent.putExtra(getString(R.string.extra_record_id), recordId);
                 startActivity(intent);
