3098cbe5beb94a0d8691d31d20da4956521a6550
Ian Field <ian.field90@gmail.com>
2016-12-19 20:13:32 +0000

    Remove google drive

--- app/src/main/java/com/ianfield/bodyscoring/activity/ViewRecordActivity.java
+++ app/src/main/java/com/ianfield/bodyscoring/activity/ViewRecordActivity.java
@@ -1,17 +1,13 @@
 package com.ianfield.bodyscoring.activity;
 
 import android.Manifest;
-import android.content.Intent;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,26 +15,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveApi;
-import com.google.android.gms.drive.MetadataChangeSet;
 import com.ianfield.bodyscoring.R;
 import com.ianfield.bodyscoring.models.Record;
 import com.ianfield.bodyscoring.models.Score;
 import com.ianfield.bodyscoring.utils.ScoreScale;
 import com.ianfield.bodyscoring.utils.Setting;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Locale;
 
@@ -49,8 +37,8 @@ import io.realm.Realm;
 /**
  * Created by Ian Field on 01/03/2016.
  */
-public class ViewRecordActivity extends AppCompatActivity
-        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class ViewRecordActivity extends AppCompatActivity {
+//        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private static final String TAG = "ViewRecordActivity";
     private static final int REQUEST_WRITE_STORAGE = 112;
 
@@ -69,7 +57,7 @@ public class ViewRecordActivity extends AppCompatActivity
     @BindView(R.id.root)
     LinearLayout root;
 
-    GoogleApiClient googleApiClient;
+//    GoogleApiClient googleApiClient;
     public static final int RESOLVE_CONNECTION_REQUEST_CODE = 1;
     private static final int REQUEST_CODE_CREATOR = 2;
     private static final int REQUEST_CODE_RESOLUTION = 3;
@@ -87,18 +75,18 @@ public class ViewRecordActivity extends AppCompatActivity
 
         setData();
 
-        if (googleApiClient == null) {
-            // Create the API client and bind it to an instance variable.
-            // We use this instance as the callback for connection and connection
-            // failures.
-            // Since no account name is passed, the user is prompted to choose.
-            googleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(Drive.API)
-                    .addScope(Drive.SCOPE_FILE)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
-        }
+//        if (googleApiClient == null) {
+//            // Create the API client and bind it to an instance variable.
+//            // We use this instance as the callback for connection and connection
+//            // failures.
+//            // Since no account name is passed, the user is prompted to choose.
+//            googleApiClient = new GoogleApiClient.Builder(this)
+//                    .addApi(Drive.API)
+//                    .addScope(Drive.SCOPE_FILE)
+//                    .addConnectionCallbacks(this)
+//                    .addOnConnectionFailedListener(this)
+//                    .build();
+//        }
     }
 
     @Override
@@ -110,9 +98,9 @@ public class ViewRecordActivity extends AppCompatActivity
                 return true;
             case R.id.action_save_chart:
                 return checkPermissionsAndSave();
-            case R.id.action_upload:
-                googleApiClient.connect();
-                return true;
+//            case R.id.action_upload:
+//                googleApiClient.connect();
+//                return true;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -166,7 +154,7 @@ public class ViewRecordActivity extends AppCompatActivity
                 return "";
             }
         });
-        set.setValueTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 3, getResources().getDisplayMetrics()));
+        set.setValueTextSize(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 5, getResources().getDisplayMetrics()));
 
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
         BarData data = new BarData(set);
@@ -178,18 +166,19 @@ public class ViewRecordActivity extends AppCompatActivity
     }
 
     private void setupChart(BarData data, double[] scoreScale) {
+        // TODO refactor for butterknife
+        float offset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 8f, getResources().getDisplayMetrics());
+        float axisFontSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12f, getResources().getDisplayMetrics());
+
         chart.setAutoScaleMinMaxEnabled(true);
         // no description text
-        chart.setDescription("");
-        chart.setNoDataTextDescription("You need to provide data for the chart.");
-
-        // enable / disable grid background
+        Description description = new Description();
+        description.setText("");
+        chart.getLegend().setEnabled(false);
+        chart.setExtraOffsets(offset, offset, offset, offset);
+        chart.setDescription(description);
         chart.setDrawGridBackground(false);
-
-        // enable touch gestures
         chart.setTouchEnabled(true);
-
-        // enable scaling and dragging
         chart.setDragEnabled(true);
         chart.setScaleEnabled(true);
 
@@ -199,25 +188,28 @@ public class ViewRecordActivity extends AppCompatActivity
         chart.setBackgroundColor(getResources().getColor(R.color.graph_background));
         chart.setDrawValueAboveBar(true);
 
-        YAxis leftAxis = chart.getAxisLeft();
-        leftAxis.setEnabled(true);
-        leftAxis.setDrawZeroLine(false);
-        leftAxis.setDrawGridLines(true);
-        leftAxis.removeAllLimitLines();
-        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
-        leftAxis.setLabelCount(scoreScale.length, false);
-        leftAxis.setDrawLabels(true);
-        leftAxis.setAxisMinValue(0);
+        YAxis yAxis = chart.getAxisLeft();
+        yAxis.setEnabled(true);
+        yAxis.setDrawZeroLine(false);
+        yAxis.setDrawGridLines(true);
+        yAxis.removeAllLimitLines();
+        yAxis.setGranularity(1f);
+        yAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        yAxis.setLabelCount(scoreScale.length, false);
+        yAxis.setDrawLabels(true);
+        yAxis.setAxisMinimum(0);
+        yAxis.setTextSize(axisFontSize);
 
         chart.getAxisRight().setEnabled(false);
 
         XAxis xAxis = chart.getXAxis();
         xAxis.setEnabled(true);
         xAxis.setDrawGridLines(true);
-        xAxis.setAxisMinValue((float) scoreScale[0]);
-        xAxis.setAxisMaxValue((float) scoreScale[scoreScale.length - 1] + 1);
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setGranularity(0.5f);
+        xAxis.setAxisMinimum((float) scoreScale[0] - .5f);
+        xAxis.setAxisMaximum((float) scoreScale[scoreScale.length - 1] + .5f);
+        xAxis.setTextSize(axisFontSize);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
 
         chart.setData(data);
 //        chart.animateX(200);
@@ -245,88 +237,88 @@ public class ViewRecordActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // save the file
-        Log.i(TAG, "GoogleApiClient connected, uploading");
+//    @Override
+//    public void onConnected(@Nullable Bundle bundle) {
+//        // save the file
+//        Log.i(TAG, "GoogleApiClient connected, uploading");
+//
+//        Drive.DriveApi.newDriveContents(googleApiClient)
+//                .setResultCallback(contentsCallback);
+//
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//        Log.d(TAG, "onConnectionSuspended: ");
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+//        // Called whenever the API client fails to connect.
+//        Log.i(TAG, "GoogleApiClient connection failed: " + connectionResult.toString());
+//        if (!connectionResult.hasResolution()) {
+//            // show the localized error dialog.
+//            GoogleApiAvailability.getInstance().getErrorDialog(ViewRecordActivity.this, connectionResult.getErrorCode(), 0).show();
+//            return;
+//        }
+//        // The failure has a resolution. Resolve it.
+//        // Called typically when the app is not yet authorized, and an authorization
+//        // dialog is displayed to the user.
+//        try {
+//            Log.d(TAG, "onConnectionFailed: " + "attempting resolution");
+//            connectionResult.startResolutionForResult(ViewRecordActivity.this, RESOLVE_CONNECTION_REQUEST_CODE);
+//        } catch (IntentSender.SendIntentException e) {
+//            Log.e(TAG, "Exception while starting resolution activity", e);
+//        }
+//    }
 
-        Drive.DriveApi.newDriveContents(googleApiClient)
-                .setResultCallback(contentsCallback);
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(TAG, "onConnectionSuspended: ");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        // Called whenever the API client fails to connect.
-        Log.i(TAG, "GoogleApiClient connection failed: " + connectionResult.toString());
-        if (!connectionResult.hasResolution()) {
-            // show the localized error dialog.
-            GoogleApiAvailability.getInstance().getErrorDialog(ViewRecordActivity.this, connectionResult.getErrorCode(), 0).show();
-            return;
-        }
-        // The failure has a resolution. Resolve it.
-        // Called typically when the app is not yet authorized, and an authorization
-        // dialog is displayed to the user.
-        try {
-            Log.d(TAG, "onConnectionFailed: " + "attempting resolution");
-            connectionResult.startResolutionForResult(ViewRecordActivity.this, RESOLVE_CONNECTION_REQUEST_CODE);
-        } catch (IntentSender.SendIntentException e) {
-            Log.e(TAG, "Exception while starting resolution activity", e);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case RESOLVE_CONNECTION_REQUEST_CODE:
-                if (resultCode == RESULT_OK) {
-                    googleApiClient.connect();
-                }
-                break;
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case RESOLVE_CONNECTION_REQUEST_CODE:
+//                if (resultCode == RESULT_OK) {
+//                    googleApiClient.connect();
+//                }
+//                break;
+//        }
+//    }
 
 
-    final private ResultCallback<DriveApi.DriveContentsResult> contentsCallback =
-            new ResultCallback<DriveApi.DriveContentsResult>() {
-                @Override
-                public void onResult(@NonNull DriveApi.DriveContentsResult result) {
-                    if (!result.getStatus().isSuccess()) {
-                        Snackbar.make(root, R.string.error_drive_upload, Snackbar.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    // Get an output stream for the contents.
-                    OutputStream outputStream = result.getDriveContents().getOutputStream();
-                    try {
-                        outputStream.write(record.toCSV().getBytes());
-                    } catch (IOException e) {
-                        Log.e(TAG, "Unable to write file contents.", e);
-                    }
-                    // Create the initial metadata - MIME type and title.
-                    // Note that the user will be able to change the title later.
-                    MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
-                            .setMimeType("text/csv")
-                            .setTitle(record.getName() + System.currentTimeMillis() +  ".csv")
-                            .build();
-                    // Create an intent for the file chooser, and start it.
-                    IntentSender intentSender = Drive.DriveApi
-                            .newCreateFileActivityBuilder()
-                            .setInitialMetadata(metadataChangeSet)
-                            .setInitialDriveContents(result.getDriveContents())
-                            .build(googleApiClient);
-                    try {
-                        startIntentSenderForResult(
-                                intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
-                    } catch (IntentSender.SendIntentException e) {
-                        Log.e(TAG, "Failed to launch file chooser.", e);
-                    }
-                }
-            };
+//    final private ResultCallback<DriveApi.DriveContentsResult> contentsCallback =
+//            new ResultCallback<DriveApi.DriveContentsResult>() {
+//                @Override
+//                public void onResult(@NonNull DriveApi.DriveContentsResult result) {
+//                    if (!result.getStatus().isSuccess()) {
+//                        Snackbar.make(root, R.string.error_drive_upload, Snackbar.LENGTH_SHORT).show();
+//                        return;
+//                    }
+//
+//                    // Get an output stream for the contents.
+//                    OutputStream outputStream = result.getDriveContents().getOutputStream();
+//                    try {
+//                        outputStream.write(record.toCSV().getBytes());
+//                    } catch (IOException e) {
+//                        Log.e(TAG, "Unable to write file contents.", e);
+//                    }
+//                    // Create the initial metadata - MIME type and title.
+//                    // Note that the user will be able to change the title later.
+//                    MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+//                            .setMimeType("text/csv")
+//                            .setTitle(record.getName() + System.currentTimeMillis() +  ".csv")
+//                            .build();
+//                    // Create an intent for the file chooser, and start it.
+//                    IntentSender intentSender = Drive.DriveApi
+//                            .newCreateFileActivityBuilder()
+//                            .setInitialMetadata(metadataChangeSet)
+//                            .setInitialDriveContents(result.getDriveContents())
+//                            .build(googleApiClient);
+//                    try {
+//                        startIntentSenderForResult(
+//                                intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
+//                    } catch (IntentSender.SendIntentException e) {
+//                        Log.e(TAG, "Failed to launch file chooser.", e);
+//                    }
+//                }
+//            };
 }
